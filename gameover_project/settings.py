89cf004cbfbf22ce5d24e95d05dc9f90f45c5b38
Kattasaky <katta.vergara.libertad@gmail.com>
2026-03-15 18:24:27 -0300

    Registro de la app tienda en settings

--- gameover_project/settings.py
+++ gameover_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tienda', #mi app creada para el proyecto
 ]
 
 MIDDLEWARE = [
