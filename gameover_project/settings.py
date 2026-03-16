2129c4c17968b1b50c5f84bce62931482dc3716f
Kattasaky <katta.vergara.libertad@gmail.com>
2026-03-16 01:36:02 -0300

    Configuración static y css base

--- gameover_project/settings.py
+++ gameover_project/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [] # carpeta para archivos estáticos
 
 MEDIA_URL = '/media/' # URL para acceder a los archivos multimedia
 MEDIA_ROOT = 'media' # carpeta donde se guardan los archivos multimedia
